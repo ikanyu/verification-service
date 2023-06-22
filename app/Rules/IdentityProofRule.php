be8bc17df08474d08962ceaf1c33723e440b3c8c
WenYu <wongwy93@gmail.com>
2023-06-23 00:51:04 +0800

    Remove comments

--- app/Rules/IdentityProofRule.php
+++ app/Rules/IdentityProofRule.php
@@ -5,15 +5,9 @@ namespace App\Rules;
 use Closure;
 use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Support\Facades\Http;
-use phpDocumentor\Reflection\PseudoTypes\True_;
 
 class IdentityProofRule implements ValidationRule
 {
-    /**
-     * Run the validation rule.
-     *
-     * @param  \Closure(string): \Illuminate\Translation\PotentiallyTranslatedString  $fail
-     */
     public function validate(string $attribute, mixed $value, Closure $fail): void
     {
         $baseUrl = 'https://dns.google';
