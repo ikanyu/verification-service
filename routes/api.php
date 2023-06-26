67a08c8ecb075ae5aca80e55d06cf3df75c288ff
WenYu <wongwy93@gmail.com>
2023-06-26 22:04:57 +0800

    Add basic files

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,5 @@ use App\Http\Controllers\Api\VerifiableDocumentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// Route::post('verifiable_documents', [VerifiableDocumentController::class, 'store']);
-
 Route::apiResource('verifiable_documents', VerifiableDocumentController::class);
 
